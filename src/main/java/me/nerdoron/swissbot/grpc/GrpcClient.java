7c36e2abe8713486ceb2e2e8a8b9498bf04c3365
nerdoron <doron.segev1803@gmail.com>
2021-03-18 12:47:02 +0200

    h

--- src/main/java/me/nerdoron/swissbot/grpc/GrpcClient.java
+++ src/main/java/me/nerdoron/swissbot/grpc/GrpcClient.java
@@ -5,10 +5,14 @@ import io.grpc.ManagedChannelBuilder;
 
 public class GrpcClient {
 	
-	public static void grpc() {
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 3020).usePlaintext().build();
-		
-		
+	public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext()
+                .build();
+        
+        
+        
+
 	}
 
 }
